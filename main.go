8a0b4291bfbf49bf99c36dbc094630c86c4ae87e
mha <jiacheng.chen2929@gmail.com>
2024-07-09 22:46:20 +0800

    2024/07/09

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/techschool/simplebank/api"
+	db "github.com/techschool/simplebank/db/sqlc"
+	"github.com/techschool/simplebank/db/util"
+	"log"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to database", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("can not start server:", err)
+	}
+}
