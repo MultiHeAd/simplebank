e85c7df7a8909e082e736e6c3509d21e807850a8
mha <jiacheng.chen2929@gmail.com>
2024-07-09 18:58:37 +0800

    Initial commit

---
+++ db/util/random.go
@@ -0,0 +1,45 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+// 创建一个全局的随机数生成器实例
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+// RandomInt generates a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + seededRand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of specified length
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney generates a random amount of money
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+// RandomCurrency generates a random currency code
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	return currencies[seededRand.Intn(len(currencies))]
+}
