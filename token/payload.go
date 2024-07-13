7b5db6de81b1380bbe77b98df71fb717d8262670
mha <jiacheng.chen2929@gmail.com>
2024-07-13 09:21:37 +0800

    add_token

---
+++ token/payload.go
@@ -0,0 +1,42 @@
+package token
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+var (
+	ErrExpiredToken = errors.New("token is expired")
+	ErrInvalidToken = errors.New("token is invalid")
+)
+
+type Payload struct {
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	IssuedAt time.Time `json:"issued_at"`
+	ExpireAt time.Time `json:"expire_at"`
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:       tokenID,
+		Username: username,
+		IssuedAt: time.Now(),
+		ExpireAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
+
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpireAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
